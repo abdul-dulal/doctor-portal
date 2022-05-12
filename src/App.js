f8f358c3d6cba7f57089a064100ab3c18ae98ac0
abdul-dulal <dulal.dpi.387364@gmail.com>
2022-05-12 23:15:09 +0600

    added navBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../src/Compnents/Navbar";
+import About from "./Compnents/pages/About";
+import Appointment from "./Compnents/pages/Appointment";
+import ContactUs from "./Compnents/pages/ContactUs";
+import Home from "./Compnents/pages/Home";
+import Login from "./Compnents/pages/Login";
+import Reviews from "./Compnents/pages/Reviews";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/review" element={<Reviews />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
